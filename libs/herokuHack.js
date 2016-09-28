bf31b2908bfd8fca8179a3f149831f29432908b1
Magmakom <magmakomer@gmail.com>
2016-09-28 17:43:46 +0300

    heroku hack fix

--- libs/herokuHack.js
+++ libs/herokuHack.js
@@ -1,11 +1,12 @@
 var config = require('./config');
 var log = require('./log')(module);
 var http = require('http');
+var https = require('https');
 
 function callApp() {
     setInterval(function() {
-        http.get(config.get('domain'), function(res){
-        });
+        if (process.env.NODE_ENV === 'production') https.get(config.get('domain'), function(res) {});
+        if (process.env.NODE_ENV === 'development') http.get(config.get('domain'), function(res) {});
     }, 300000); // every 5 minutes (300000)
 }
 
